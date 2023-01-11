9f6580f64c0f8155149609dadd28bcb39227a5e7
igor <igorbelikov2009@yandex.ru>
2023-01-11 16:22:50 +0300

    anotation app

--- src/apollo/todos.js
+++ src/apollo/todos.js
@@ -19,3 +19,21 @@ export const ADD_TODO = gql`
     }
   }
 `;
+
+export const UPDATE_TODO = gql`
+  mutation UpdateTodo($id: ID!, $completed: Boolean) {
+    updateTodo(id: $id, completed: $completed) {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+export const DELETE_TODO = gql`
+  mutation DeleteTodo($id: ID!) {
+    removeTodo(id: $id) {
+      id
+    }
+  }
+`;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,27 +1,54 @@
 import { VStack } from "@chakra-ui/react";
 import { Spinner } from "@chakra-ui/react";
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 
 import TodoItem from "./TodoItem";
 import TotalCount from "./TotalCount";
-import { ALL_TODO } from "../apollo/todos";
+import { ALL_TODO, UPDATE_TODO, DELETE_TODO } from "../apollo/todos";
 
 const TodoList = () => {
   // useQuery подразумевает запрос сразу, запрос произойдёт автоматически,
   // нам не надо писать useEffect(), ничего.
   const { data, loading, error } = useQuery(ALL_TODO);
 
+  // Так как, это мутация, элементы которой уже существуют у нас в кэше,
+  // дополнительно преобразовывать ничего не нужно, кэш обновится автоматически.
+  // toggleTodo будем передавать вниз, в <TodoItem />
+  const [toggleTodo, { error: updateError }] = useMutation(UPDATE_TODO);
+  // removeTodo будем передавать вниз, в <TodoItem />
+  const [removeTodo, { error: deleteError }] = useMutation(DELETE_TODO, {
+    // Снова используем метод  update(),
+    update(cache, { data: { removeTodo } }) {
+      // У кэша используем метод: модификация. Для модификации мы говорим, что мы хотим
+      // поменять конкретные поля, и эти поля называются allTodos. Аллиас мы не можем
+      // использовать, только оригинал со страницы todos.js, строка 5.
+      cache.modify({
+        fields: {
+          // allTodos принимает параметрами текущие тудушки, если их нет то пустой массив.
+          allTodos(currentTodos = []) {
+            // дальше мы должны отфильтровать наш currentTodos с этим id-шником.
+            // смотрим в кэш, там тудушки хранятся рефами, поэтому нам надо вернуть
+            // наши тудушки, но отфильтрованные.
+            // __ref:"Todo:2" так выглядит наша тудушка в кэше, смотри devtools apollo, cache.
+            // Выражение ${removeTodo.id} - это наш id.
+            return currentTodos.filter((todo) => todo.__ref !== `Todo:${removeTodo.id}`);
+          },
+        },
+      });
+    },
+  });
+
   if (loading) {
     return <Spinner />;
   }
 
-  if (error) return <h2>Error!!!</h2>;
+  if (error || updateError || deleteError) return <h2>Error!!!</h2>;
 
   return (
     <>
       <VStack spacing={2} mt={4}>
         {data.todos.map((todo) => (
-          <TodoItem key={todo.id} {...todo} />
+          <TodoItem key={todo.id} onToggle={toggleTodo} {...todo} onDelete={removeTodo} />
         ))}
       </VStack>
       <TotalCount />
