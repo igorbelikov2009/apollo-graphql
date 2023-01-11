8ae2d9ba4777cd3b4dfd63ca895a6d040a60c727
igor <igorbelikov2009@yandex.ru>
2023-01-11 13:27:34 +0300

    AddTodo

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,26 @@
 import { VStack } from "@chakra-ui/react";
 import { Spinner } from "@chakra-ui/react";
+import { useQuery } from "@apollo/client";
 
 import TodoItem from "./TodoItem";
 import TotalCount from "./TotalCount";
+import { ALL_TODO } from "../apollo/todos";
 
 const TodoList = () => {
-  const todos = [];
+  // useQuery подразумевает запрос сразу, запрос произойдёт автоматически,
+  // нам не надо писать useEffect(), ничего.
+  const { data, loading, error } = useQuery(ALL_TODO);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (error) return <h2>Error!!!</h2>;
 
   return (
     <>
       <VStack spacing={2} mt={4}>
-        {todos.map((todo) => (
+        {data.todos.map((todo) => (
           <TodoItem key={todo.id} {...todo} />
         ))}
       </VStack>
